c7b5f9c59bee322d148e496408a6042684c6c05f
ALPHA <alpharajasa@gmail.com>
2022-05-20 15:37:24 +0700

    Build test

--- app/src/main/java/com/example/chapter_7_allminitask/MainActivity.kt
+++ app/src/main/java/com/example/chapter_7_allminitask/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.chapter_7_allminitask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.viewbinding.BuildConfig
+import com.example.chapter_7_allminitask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Toast.makeText(this, BuildConfig.BUILD_TYPE, Toast.LENGTH_SHORT).show()
+
     }
 }
